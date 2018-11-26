67eb701468c8b48f8988a547a53e71f840497d84
roc <roc@ninja.org>
2018-11-26 16:58:37 -0500

    add account

--- constant/account.go
+++ constant/account.go
@@ -1,7 +1,16 @@
 package constant
 
 type Account struct {
-	address   Address
-	utxo      MerkleTree // maybe
-	variables map[string]interface{}
+	// account unique identifier, 32-byte address
+	address Address
+
+	// account state
+	state map[string]interface{}
+
+	// account utxo
+	utxo MerkleTree
+
+	// can be controlled either by a sig or multisig
+	signatures []Signature
+	sigQuorum  int
 }
